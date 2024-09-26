4a991422bdc533e4fcf9234a79efd62531d0fbf7
S8352 <surya8352@gmail.com>
2024-09-26 09:38:49 +0530

    update

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "../styles/globals.css"
-import TransitionProvider from "@components/transitionProvider";
+import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +18,12 @@ export default function RootLayout({
   return (
     <html lang="en">
     <body className={inter.className}>
-      <TransitionProvider>{children}</TransitionProvider>
+      <div className="h-24 top-0 fixed bg-white z-50">
+          <Navbar />
+        </div>
+        <div className="mt-24">
+          {children}
+        </div>
     </body>
   </html>
   );
